62142d31db3d582f750810fa4369c92a21614709
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-02 11:58:42 -0300

    :sparkles: feat: Adds persistence markers on Pessoa entity.

--- src/main/java/domain/Pessoa.java
+++ src/main/java/domain/Pessoa.java
@@ -1,12 +1,19 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
   @Serial
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String name;
   private String email;
